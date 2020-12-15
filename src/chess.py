18cb724a871e7844d48f4155134c9294779e82a6
JosA(c) LuAs Cunha Rodrigues <up202008462@feup-b207d36.fe.up.pt>
2020-12-15 15:25:44 +0000

    Added pawn movement

--- src/chess.py
+++ src/chess.py
@@ -36,7 +36,33 @@ def inside_board(mouse):
 
 def hitbox(mouse):
     '''Returns the square where the mouse clicked'''
+    #print((mouse[0]//64 - 1, mouse[1]//64 - 1))
     return (mouse[0]//64 - 1, mouse[1]//64 - 1)
+
+def possible_moves(coords):
+    '''Returns a list of tuples with the possible moves for a certain piece on the board'''
+    piece = board[coords[1]][coords[0]]
+    res = []
+    #print(coords)
+    if piece == wpawn and coords[1] > 0:
+        res.append((coords[1] - 1, coords[0]))
+        if coords[1] == 6: res.append((coords[1] - 2, coords[0])) #first move
+        if board[coords[1] - 1][coords[0] + 1] != 0: res.append((coords[1] - 1, coords[0] + 1)) #captures
+        if board[coords[1] - 1][coords[0] - 1] != 0: res.append((coords[1] - 1, coords[0] - 1))
+    if piece == bpawn: 
+        res.append((coords[1] + 1, coords[0]))
+        if coords[1] == 1: res.append((coords[1] + 2, coords[0])) #first move
+        if board[coords[1] + 1][coords[0] + 1] != 0: res.append((coords[1] + 1, coords[0] + 1)) #captures
+        if board[coords[1] + 1][coords[0] - 1] != 0: res.append((coords[1] + 1, coords[0] - 1))
+    print(res)
+    return res
+    
+def draw_moves(coords_list):
+    #print(coords_list)
+    for coords in coords_list:
+        pygame.draw.circle(win, (0,0,0), (coords[1] * 64 + 96, coords[0] * 64 + 96), 7)
+        
+possible_moves((1,2))
 
 run = True
 while run:
@@ -48,19 +74,20 @@ while run:
             run = False
         if event.type == pygame.MOUSEBUTTONDOWN:
             if not inside_board(mouse): continue
-            if selected_check and hitbox(mouse) != selected: #select the square to move to
+            if selected_check and hitbox(mouse) != selected and hitbox(mouse)[::-1] in possible_moves(selected): #select the square to move to
                 new = hitbox(mouse)
                 board[new[1]][new[0]] = board[selected[1]][selected[0]]
                 board[selected[1]][selected[0]] = 0
                 selected_check = False
+            elif selected_check and hitbox(mouse) == selected: selected_check = False
             else: #select a piece to move
                 selected = hitbox(mouse)
                 if board[selected[1]][selected[0]] != 0: selected_check = True
-            
     
     win.blit(board_img, (0,0))
     
     draw_board(board)
+    if selected_check: draw_moves(possible_moves(selected))
     pygame.display.update()
 
 pygame.quit()
